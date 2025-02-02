4be52ffc7f46087067da97e3f5f8ab7d8d760e55
Thomas Cormican <thomascormican123@gmail.com>
2025-02-02 21:36:54 +0000

    Database steup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://hktcsccavm.ufs.sh/f/ufhDLWrGmIUJmPeZnxpoaCZdJNwtKSp2987hbBAn3UTqFjRP",
@@ -14,10 +15,17 @@ const mockImages = [...mockUrls, ...mockUrls, ...mockUrls].map(
   }),
 );
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
         {mockImages.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} />
