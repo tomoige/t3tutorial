7c866a2e73a842252f3a64f5157196460aa0a3b0
Thomas Cormican <thomascormican123@gmail.com>
2025-02-02 21:53:13 +0000

    db changes

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -7,5 +7,5 @@ import * as schema from "./schema";
 
 config({ path: ".env" }); // or .env.local
 
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon(process.env.POSTGRES_URL!);
 export const db = drizzle(sql, { schema });
