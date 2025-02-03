13e9924bd020bb2f7c625f6468bb1396e01150b9
Thomas Cormican <thomascormican123@gmail.com>
2025-02-03 18:24:49 +0000

    force dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic";
+
 const mockUrls = [
   "https://hktcsccavm.ufs.sh/f/ufhDLWrGmIUJmPeZnxpoaCZdJNwtKSp2987hbBAn3UTqFjRP",
   "https://hktcsccavm.ufs.sh/f/ufhDLWrGmIUJDDclrvGjl8Wm6S2Mw5JoaitzLsEXUKkVerZq",
