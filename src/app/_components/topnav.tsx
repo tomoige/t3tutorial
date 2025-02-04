86ac1aabe341f63534946cb763aeada4838136cf
Thomas Cormican <thomascormican123@gmail.com>
2025-02-04 15:40:16 +0000

    Add auth

---
+++ src/app/_components/topnav.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
+function TopNav() {
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
+      <div>Gallery</div>
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  );
+}
+
+export default TopNav;
