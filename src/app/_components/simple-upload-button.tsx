1504e184f0ca46f84610764b9c12c388993f332c
Thomas Cormican <thomascormican123@gmail.com>
2025-02-13 21:19:37 +0000

    Add toast

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -2,7 +2,7 @@
 
 import { useUploadThing } from "~/utils/uploadthing";
 import { useRouter } from "next/navigation";
-import { toast } from "sonner";
+
 import { useToast } from "~/hooks/use-toast";
 
 // inferred input off useUploadThing
@@ -45,6 +45,27 @@ function UploadSVG() {
         strokeLinecap="round"
         strokeLinejoin="round"
         d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+      />
+    </svg>
+  );
+}
+
+function LoadingSpinnerSVG() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="white"
+    >
+      <path
+        d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
+        opacity=".25"
+      />
+      <path
+        d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z"
+        className="spinner_ajPY"
       />
     </svg>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,10 +39,7 @@ export default function RootLayout({
           <div className="grid h-screen grid-rows-[auto,1fr]">
             <TopNav />
 
-            <main className="overflow-y-scroll">
-              <Button>A button</Button>
-              {children}
-            </main>
+            <main className="overflow-y-scroll">{children}</main>
           </div>
           {modal}
           <div id="modal-root" />
